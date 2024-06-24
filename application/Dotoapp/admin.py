689183efd5efeede6278ad8e98037cdc7e2eda07
Arkan Leki <arkan.leki@gmail.com>
2024-06-24 23:12:15 +0300

    tester changes

--- application/Dotoapp/admin.py
+++ application/Dotoapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Category, Transaction
 
-# Register your models here.
+# models in Admin dashboard is Highly Required for backend dev
+admin.site.register(Transaction)
+admin.site.register(Category)
