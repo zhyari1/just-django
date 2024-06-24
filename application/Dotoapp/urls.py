0bdf1774d59a628f965fbc4d05d612ddfa4dc431
arkan-leki <arkan.leki@gmail.com>
2024-06-24 23:08:49 +0300

    Update urls.py

--- application/Dotoapp/urls.py
+++ application/Dotoapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 
-urlpatterns = [path(''  ,  views.view , name ="Home") , 
-                path('/transaction', views.your_view, name='transaction'),
-
-               ]
+urlpatterns = [
+  path(''  ,  views.view , name ="Home") , 
+  path('/transaction', views.your_view, name='transaction'),
+]
