d69fd8c0d0d7386065d4a4532fe8b2d33721dbf5
zhyari1 <zhyarosman57@gmail.com>
2024-06-26 17:06:45 +0300

    Update filter.py

--- application/Dotoapp/filter.py
+++ application/Dotoapp/filter.py
@@ -1,15 +1,21 @@
 import django_filters
-from .models import Transaction
+from django import forms
+from .models import Transaction, Category
 
 class TransactionFilter(django_filters.FilterSet):
     start_date = django_filters.DateFilter(field_name='date', lookup_expr='gte')
     end_date = django_filters.DateFilter(field_name='date', lookup_expr='lte')
+    amount = django_filters.RangeFilter() # this is done doesn't need any thing 
+    min_amount = django_filters.NumberFilter(field_name='amount', lookup_expr='gte')
 
     class Meta:
         model = Transaction
         fields = {
+            'sender_account': ['exact'],
+            'receiver_account': ['exact'],
+            'amount': ['exact', 'range'],
             'transaction_type': ['exact'],
             'category': ['exact'],
-            'amount': ['gte', 'lte'],
+            'description': ['icontains'],
             'date': ['exact', 'year', 'month', 'day'],
         }
